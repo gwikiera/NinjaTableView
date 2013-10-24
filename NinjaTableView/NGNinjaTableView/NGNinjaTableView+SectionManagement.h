bc6298a4dcee915cf09072b3b75f876c266291fe
Krzysztof Profic <kpr@kpr.local>
2013-10-24 11:38:34 +0200

    Formatting and License

--- NinjaTableView/NGNinjaTableView/NGNinjaTableView+SectionManagement.h
+++ NinjaTableView/NGNinjaTableView/NGNinjaTableView+SectionManagement.h
@@ -1,10 +1,24 @@
-//
-//  NGNinjaTableView+SectionManagement.h
-//  NinjaTableView
-//
-//  Created by Krzysztof Profic on 19/09/2013.
-//  Copyright (c) 2013 Krzysztof Profic. All rights reserved.
-//
+/*
+ * Copyright (c) 2013 Krzysztof Profic
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #import "NGNinjaTableView.h"
 
@@ -18,6 +32,7 @@
 
 
 @protocol NGSectionManagementDelegate <NSObject>    // object that may implement this protocol is UITableViews' "delegate"
+
 @optional
 // folding / unfolding delegate methods
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSectionWhenFolded:(NSInteger)section;  // 0 by default
@@ -36,10 +51,12 @@
 - (NSIndexSet *)tableView:(UITableView *)tableView willStartShowingSections:(NSIndexSet *)sections animated:(BOOL)animated;
 - (void)tableView:(UITableView *)tableView didStartShowingSections:(NSIndexSet *)sections animated:(BOOL)animated;
 - (void)tableView:(UITableView *)tableView didFinishShowingSections:(NSIndexSet *)sections animated:(BOOL)animated;
+
 @end
 
 
 @interface NGNinjaTableView (SectionManagement)
+
 @property (nonatomic, unsafe_unretained) BOOL allowsUnfoldingOnMultipleSections; // YES by default, setter may be called when the tableView view is already loaded, if set to NO all sections become closed
 @property (nonatomic, readonly) NSIndexSet * foldedSectionsIndexSet;
 @property (nonatomic, readonly) NSIndexSet * hiddenSectionsIndexSet;
